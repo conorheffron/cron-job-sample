9c776884f7f2ffed8dd7a7226ce9d520ec2fdeac
Conor H <conor.heffron@gmail.com>
2024-01-05 01:00:38 +0000

    Update config and dependencies in build file to resolve security vulnerabilities detected by code scan ( https://github.com/conorheffron/cron-job-sample/security/dependabot )

--- src/main/java/com/test/cron/job/FileFeedJob.java
+++ src/main/java/com/test/cron/job/FileFeedJob.java
@@ -12,12 +12,11 @@ public class FileFeedJob {
 	@Autowired
 	private FileService fileService;
 
-//	@Scheduled(fixedDelay = 10000) // every 10 secs and does NOT wait for last execution to finish
+	@Scheduled(fixedDelay = 10000) // every 10 secs and does NOT wait for last execution to finish
 //	@Scheduled(cron = "0 45 9 * * MON-FRI") // mon to fri at 945am
 //	@Scheduled(cron = "${cron.job.time}") // externalize job time...
 //	@Scheduled(cron = "*/7 * * * * *") // every 7 seconds
-	
-	@Scheduled(fixedRate = 10000) // every 10 secs and waits for last execution to finish
+//	@Scheduled(fixedRate = 10000) // every 10 secs and waits for last execution to finish
 	public void execute() {
 		fileService.readFileLineByLine();
 	}
