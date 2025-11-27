ea96615c2827f92c44c7684fdb64c98bb5093cf9
Conor H <conor.heffron@gmail.com>
2025-11-27 12:45:48 +0000

    Render Home View with JSP

--- src/main/java/net/ironoc/cron/Application.java
+++ src/main/java/net/ironoc/cron/Application.java
@@ -2,11 +2,19 @@ package net.ironoc.cron;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
